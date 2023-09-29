2d995b142b05ccc8f4aa2d8a2a07a3f381c3e923
Altaf <imaltaf@proton.me>
2023-09-29 12:22:44 +0530

    TEST 001

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230927.230513';
+const cacheName = 'chirpy-20230929.121905';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
